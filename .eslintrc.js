f426c18edf1497d3be91992e3302ddda00765708
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-01 15:01:10 -0400

    remove ember-cli-eslint and reenable extends.

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
         node: true
       },
       plugins: ["node"],
-      // extends: ["plugin:node/recommended"],
+      extends: ["plugin:node/recommended"],
       rules: {}
     }
   ]
